257578de69e37521183125396c8fad5d1ea72429
philpo2121 <fortnite.philpo1@gmail.com>
2026-04-09 19:13:03 +0200

    h

--- patch_map.sh
+++
@@ -1,5 +0,0 @@
-sed -i '' 's/        Map {/    private var simplifiedCoordinates: [CLLocationCoordinate2D] {\n        guard coordinates.count > 30 else { return coordinates }\n        let step = coordinates.count \/ 30\n        var result: [CLLocationCoordinate2D] = []\n        for i in stride(from: 0, to: coordinates.count, by: step) {\n            result.append(coordinates[i])\n        }\n        if let last = coordinates.last {\n            result.append(last)\n        }\n        return result\n    }\n\n    var body: some View {\n        Map {/' Ascent/ActivityCardView.swift
-
-sed -i '' 's/MapPolyline(coordinates: coordinates)/MapPolyline(coordinates: simplifiedCoordinates)/' Ascent/ActivityCardView.swift
-
-sed -i '' 's/\.mapStyle(.standard(elevation: .realistic, pointsOfInterest: .excludingAll))/\.mapStyle(.standard(elevation: .flat, pointsOfInterest: .excludingAll))\n        \.mapControlVisibility(.hidden)/' Ascent/ActivityCardView.swift
--- patch_timer.sh
+++
@@ -1,7 +0,0 @@
-sed -i '' 's/if isRunning && !gpsManager.isAutoPaused { timeElapsed += 1 }/if isRunning \&\& !gpsManager.isAutoPaused { \
-                timeElapsed += 1 \
-                appState.trackerElapsedSeconds = timeElapsed\
-            }\
-            appState.trackerDistanceKm = gpsManager.distance \/ 1000.0\
-            appState.trackerElevationGain = gpsManager.elevationGain\
-            appState.isTrackerPaused = !isRunning || gpsManager.isAutoPaused/' Ascent/LiveRecordView.swift
